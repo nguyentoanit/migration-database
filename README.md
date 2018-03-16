0dcd10825f4d2f6a4330ef2ae4e89a084bbcf420
Fuji_Toan <toan.nguyen@fujitechjsc.com>
2018-03-16 16:36:44 +0700

    migration mysql and postgresql

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# migration-database
+# Database migration tool
+## Author
+- Name: **Toan Nguyen**
+- Github: https://github.com/nguyentoanit
+- Facebook: https://www.facebook.com/nguyentoandev
+
+## Options
+Migration tool supports the following options, which can be specified on the command line
+
+For information about option:
+
+| Option                   | Description                                                                               |
+|--------------------------|-------------------------------------------------------------------------------------------|
+| **-pt** or **--path**    | Path of sql folder. Place contains sql file.Default: sql                                  |
+| **-h** or **--host**     | Specifies the host name of the machine on which the server is running. Default: localhost |
+| **-d** or **--database** | The database to use.. Default: postgres                                                   |
+| **-u** or **--username** | User name to use when connecting to server. Default: postgres                             |
+| **-p** or **--password** | Password to use when connecting to server. Default: null                                  |
+| **-P** or **--port**     | TCP/IP port number for connection. Default: 5432                                          |
+| **-t** or **--type**     | Type of connection. There are two type of conection (mysql and psql) Default: psql.       |
---
+++ database-migration.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#SQL file format: YYYYMMDDHHII_XXXXX.sql
+#Set default connection
+path="sql"
+host="localhost"
+database="postgres"
+username="postgres"
+password=""
+type="psql"
+port="5432"
+
+while test $# -gt 0; do
+           case "$1" in
+                -pt|--path)
+                    shift
+                    path=$1
+                    shift
+                    ;;
+                -h|--host)
+                    shift
+                    host=$1
+                    shift
+                    ;;
+                -d|--database)
+                    shift
+                    database=$1
+                    shift
+                    ;;
+                -u|--username)
+                    shift
+                    username=$1
+                    shift
+                    ;;
+                -p|--password)
+                    shift
+                    password=$1
+                    shift
+                    ;;
+                -t|--type)
+                    shift
+                    type=$1
+                    shift
+                    ;;
+                -P|--port)
+                    shift
+                    port=$1
+                    shift
+                    ;;
+          esac
+  done  
+
+cd $path
+ls *.sql > list
+
+#Check file log file
+if [ -f "sql.log" ]
+then
+    value=`cat sql.log`
+else
+    value=""
+fi
+
+# Check type of connection
+if [ "$type" == "psql" ]
+then
+    while read line; do
+    if [ "$line" \> "$value" ]
+    then
+        #Run script from file command
+        export PGPASSWORD=$password && psql -h $host -U $username -d $database -p $port -a -f $line
+    fi
+    done < list
+elif [ "$type" == "mysql" ]
+then
+    while read line; do
+    if [ "$line" \> "$value" ]
+    then
+        #Run script from file command
+        mysql -h $host -u $username -D $database -P $port -p$password < $line
+    fi
+    done < list
+fi
+
+tail -n 1 list > sql.log
+rm list
